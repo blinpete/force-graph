2113779d8bf252e4b5dd7f7430f5260c43814719
Peter <pete.blinov@yandex.ru>
2024-10-21 23:42:32 +0300

    cleanup, upd router

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router  from './router'
+
+// import '@unocss/reset/tailwind.css'
+import './assets/style.css'
+// import 'uno.css'
 
 const app = createApp(App)
-
 app.use(router)
-
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+// import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div>
+      <h1>fogr</h1>
+      <p>Will you turn text into force-directed graph or build with hands?</p>
+    </div>
+    <RouterLink to="/me">Mermaid</RouterLink>
+    <RouterLink to="/ex">Excalidraw</RouterLink>
   </main>
 </template>
